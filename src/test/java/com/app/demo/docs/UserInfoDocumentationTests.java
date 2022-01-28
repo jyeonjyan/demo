ab68f02efcf32486b7e83b9efc904a3c34887730
johnjihwan <s20062@gsm.hs.kr>
2022-01-28 15:48:45 +0900

    Add savepoint

--- src/test/java/com/app/demo/docs/UserInfoDocumentationTests.java
+++ src/test/java/com/app/demo/docs/UserInfoDocumentationTests.java
@@ -3,31 +3,32 @@ package com.app.demo.docs;
 import com.app.demo.demo.controller.UserInfoController;
 import com.app.demo.demo.dto.UserInfoDto;
 import com.app.demo.demo.service.UserInfoService;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static com.app.demo.util.ApiDocumentUtils.getDocumentRequest;
 import static com.app.demo.util.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UserInfoController.class)
+@AutoConfigureMockMvc
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
 public class UserInfoDocumentationTests {
 
@@ -39,9 +40,6 @@ public class UserInfoDocumentationTests {
 
     @MockBean
     private UserInfoService userInfoService;
-
-    @Autowired
-    private WebTestClient webTestClient;
 
 
     @Test
@@ -66,7 +64,7 @@ public class UserInfoDocumentationTests {
 
         // Then
         result.andExpect(status().isOk())
-                .andDo(document("userinfo-create",
+                .andDo(MockMvcRestDocumentationWrapper.document("userinfo-create",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         requestFields(
@@ -75,16 +73,6 @@ public class UserInfoDocumentationTests {
                                 fieldWithPath("username").type(JsonFieldType.STRING).description("사용자이름")
                         )
                 ));
-
     }
 
-    @Test
-    @DisplayName(value = "create userinfo with rest docs api")
-    void create_userinfo(){
-        webTestClient.post().uri("/")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody();
-    }
 }
