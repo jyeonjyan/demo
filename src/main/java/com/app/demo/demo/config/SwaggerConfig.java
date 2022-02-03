52832b3c03d0f14d9145e76f94e8e0dd0801b789
johnjihwan <s20062@gsm.hs.kr>
2022-02-03 18:09:43 +0900

    Update tag

--- src/main/java/com/app/demo/demo/config/SwaggerConfig.java
+++ src/main/java/com/app/demo/demo/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/app/demo/demo/controller/UserInfoController.java
+++ src/main/java/com/app/demo/demo/controller/UserInfoController.java
@@ -3,12 +3,14 @@ package com.app.demo.demo.controller;
 import com.app.demo.demo.dto.UserInfoDto;
 import com.app.demo.demo.entity.UserInfo;
 import com.app.demo.demo.service.UserInfoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "demo", description = "demo controller API")
 @RestController
 @RequestMapping("/v1/api/demo/")
 @RequiredArgsConstructor
