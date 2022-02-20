033e9772d97c5ffd70f6ed295bfb387f6f05af13
johnjihwan <s20062@gsm.hs.kr>
2022-02-21 04:34:51 +0900

    Add listResult example

--- src/main/java/com/app/demo/demo/service/UserInfoService.java
+++ src/main/java/com/app/demo/demo/service/UserInfoService.java
@@ -5,8 +5,13 @@ import com.app.demo.demo.entity.UserInfo;
 import com.app.demo.demo.repository.UserInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserInfoService {
 
@@ -15,6 +20,14 @@ public class UserInfoService {
     public UserInfoDto.UserInfoResponseDto createUserInfo(UserInfoDto.UserInfoRequestDto userInfoRequestDto){
         UserInfo savedUserInfo = userInfoRepository.save(UserInfoDto.UserInfoRequestDto.toEntity(userInfoRequestDto));
         return UserInfoDto.UserInfoResponseDto.of(savedUserInfo);
+    }
+
+    public List<UserInfoDto.UserInfoResponseDto> getAllUserInfo(){
+        final List<UserInfo> userInfoList = userInfoRepository.findAll();
+
+        return userInfoList.stream()
+                .map(UserInfoDto.UserInfoResponseDto::of)
+                .collect(Collectors.toList());
     }
 
 }
