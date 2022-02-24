1f1d2eec5333f98ac0a036fe96b24655583c5b4a
johnjihwan <s20062@gsm.hs.kr>
2022-02-24 14:50:49 +0900

    Demo 수준으로 격리

--- src/main/java/com/app/demo/demo/response/ListResultStrategy.java
+++
@@ -1,2 +0,0 @@
-package com.app.demo.demo.response;public class ListResultStrategy {
-}
--- src/main/java/com/app/demo/demo/response/ResponseStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.app.demo.demo.response;
-
-import org.springframework.http.ResponseEntity;
-
-public interface ResponseStrategy {
-    ResponseEntity<Object> createResponse(Object createdId);
-}
