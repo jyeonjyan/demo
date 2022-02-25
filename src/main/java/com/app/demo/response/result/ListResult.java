fc7ffb759fa36cc0d98dbeb700f625de8fe4fe35
johnjihwan <s20062@gsm.hs.kr>
2022-02-25 15:56:50 +0900

    Add result class 로직만 draft

--- src/main/java/com/app/demo/response/result/ListResult.java
+++ src/main/java/com/app/demo/response/result/ListResult.java
@@ -1,2 +1,16 @@
-package com.app.demo.response.result;public class ListResult {
+package com.app.demo.response.result;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ListResult<T> {
+    private final Result<List<T>> result;
+    private final int size;
+
+    public ListResult(Result<List<T>> result, int size) {
+        this.result = result;
+        this.size = size;
+    }
 }
--- src/main/java/com/app/demo/response/result/Result.java
+++ src/main/java/com/app/demo/response/result/Result.java
@@ -3,10 +3,10 @@ package com.app.demo.response.result;
 import lombok.Data;
 
 @Data
-public class SingleResult<T> {
+public class Result<T> {
     private T data;
 
-    public SingleResult(T data) {
+    public Result(T data) {
         this.data = data;
     }
 
