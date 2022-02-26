dc35b2b04477fb2b596f0570662ae92d66cae1a7
johnjihwan <s20062@gsm.hs.kr>
2022-02-26 17:04:33 +0900

    Update

--- src/main/java/com/app/demo/response/result/Result.java
+++ src/main/java/com/app/demo/response/result/Result.java
@@ -1,13 +1,34 @@
 package com.app.demo.response.result;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-public class Result<T> {
-    private T data;
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+public class
+Result<T> {
+    private final List<T> data;
+
+    /**
+     * Single Result:: minimal result
+     *
+     * @param data - single data
+     */
     public Result(T data) {
-        this.data = data;
+        // create this.data to arrayList
+        this.data = new ArrayList<>();
+
+        if (data == null) throw new IllegalArgumentException("Result data can not be null.");
+        this.data.add(data);
     }
 
+    /**
+     * List Result:: list result
+     *
+     * @param data - list data
+     */
+    public Result(List<T> data) {
+        this.data = data;
+    }
 }
